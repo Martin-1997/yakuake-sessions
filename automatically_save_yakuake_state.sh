79356752a4d93c10ebded87d15e91c80c52d3f0f
Martin <zeitler.martin@protonmail.ch>
2022-11-30 15:05:55 +0100

    Added feature for the script to set a interval for storing the yakuake session data

--- automatically_save_yakuake_state.sh
+++ automatically_save_yakuake_state.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 filename="yakuake_state.txt"
-python ysess.py -o $filename --force-overwrite
+sleep_time=5
+
+while true
+do
+    python ysess.py -o $filename --force-overwrite
+    sleep $sleep_time
+done
